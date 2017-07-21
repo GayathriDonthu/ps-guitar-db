a47afc6e2901a7e72b76a74c91321d0352d78ddc
GayathriDonthu <ggayathri29@gmail.com>
2017-07-21 20:02:09 +0530

    Spring with Data JPA version 1.2

--- src/main/java/com/guitar/db/repository/LocationJpaRepository.java
+++ src/main/java/com/guitar/db/repository/LocationJpaRepository.java
@@ -1,9 +1,21 @@
 package com.guitar.db.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.guitar.db.model.Location;
 
 public interface LocationJpaRepository extends JpaRepository<Location, Long> {
+
+	List<Location> findByStateIgnoreCaseStartingWith(String stateName);
+
+	List<Location> findByStateNotLikeOrderByStateDesc(String stateName);
+
+	List<Location> findByStateIsOrCountryEquals(String value1, String value2);
+
+	List<Location> findByStateNot(String value1);
+
+	Location findFirstByStateIgnoreCaseStartingWith(String stateName);
 
 }
--- src/test/java/com/guitar/db/ManufacturerPersistenceTests.java
+++ src/test/java/com/guitar/db/ManufacturerPersistenceTests.java
@@ -12,13 +12,17 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.guitar.db.model.Manufacturer;
+import com.guitar.db.repository.ManufacturerJpaRepository;
 import com.guitar.db.repository.ManufacturerRepository;
 
-@ContextConfiguration(locations={"classpath:com/guitar/db/applicationTests-context.xml"})
+@ContextConfiguration(locations = { "classpath:com/guitar/db/applicationTests-context.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ManufacturerPersistenceTests {
 	@Autowired
 	private ManufacturerRepository manufacturerRepository;
+
+	@Autowired
+	private ManufacturerJpaRepository manufacturerJpaRepository;
 
 	@Test
 	public void testGetManufacturersFoundedBeforeDate() throws Exception {
@@ -34,7 +38,18 @@ public class ManufacturerPersistenceTests {
 
 	@Test
 	public void testGetManufacturersThatSellModelsOfType() throws Exception {
-		List<Manufacturer> mans = manufacturerRepository.getManufacturersThatSellModelsOfType("Semi-Hollow Body Electric");
+		List<Manufacturer> mans = manufacturerRepository
+				.getManufacturersThatSellModelsOfType("Semi-Hollow Body Electric");
 		assertEquals(1, mans.size());
 	}
+
+	@Test
+	public void testTrueAndFalse() throws Exception {
+		List<Manufacturer> mans = manufacturerJpaRepository.findByActiveTrue();
+		assertEquals("Fender Musical Instruments Corporation", mans.get(0).getName());
+
+		mans = manufacturerJpaRepository.findByActiveFalse();
+		assertEquals("Gibson Guitar Corporation", mans.get(0).getName());
+	}
+
 }
